94e523ddd7e152cc4e05dcd0cc2f3e7e40ea47d3
Luiz Ferreira <Miguel@MacBook-Pro-de-Marcela.local>
2024-07-31 19:56:54 -0300

    fix: adding correct work hours and get page element

--- content.js
+++ content.js
@@ -1,25 +1,50 @@
-function extractDates() {
-  // Exemplos de como extrair datas da página web
-  // Altere o seletor de acordo com a estrutura da página
-  const dateElements = document.querySelectorAll('.date-selector');
-  if (dateElements.length < 2) return null;
+function extractTimes() {
+  const timeElements = document.querySelectorAll('.MuiTypography-body1');
+  if (timeElements.length < 1) return null;  // Agora precisamos de pelo menos 1 horário
 
-  const dates = Array.from(dateElements).slice(0, 2).map(el => new Date(el.innerText));
-  return dates;
+  const times = Array.from(timeElements).slice(0, 1).map(el => parseTime(el.innerText));
+  return times;
 }
 
-function calculateDateDifference(dates) {
-  if (dates.length < 2) return null;
-  const diffTime = Math.abs(dates[1] - dates[0]);
+function parseTime(timeStr) {
+  const timeParts = timeStr.match(/(\d{2})h (\d{2})m (\d{2})s/);
+  if (!timeParts) return null;
+
+  const hours = parseInt(timeParts[1], 10);
+  const minutes = parseInt(timeParts[2], 10);
+  const seconds = parseInt(timeParts[3], 10);
+
+  // Use the current date for the date part and set the time part
+  const date = new Date();
+  date.setHours(hours, minutes, seconds, 0);
+
+  return date;
+}
+
+function calculateTimeDifference(times) {
+  if (times.length < 1) return null;
+
+  const currentTime = new Date();  // Hora atual
+  const startTime = times[0];      // Primeiro horário extraído
+
+  const diffTime = Math.abs(currentTime - startTime);
   const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
   const diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
-  return { hours: diffHours, minutes: diffMinutes };
+
+  const newDate = new Date();
+  newDate.setHours(startTime.getHours() + 9)
+  newDate.setMinutes(startTime.getMinutes() + 48);
+  const diffLeavingHours = newDate.getHours();
+  const diffLeavingMinutes = newDate.getMinutes();
+
+  return { hours: diffHours, minutes: diffMinutes, leavingHours: diffLeavingHours, leavingMinutes: diffLeavingMinutes }
+
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'calculateDateDifference') {
-    const dates = extractDates();
-    const difference = calculateDateDifference(dates);
+  if (request.action === 'calculateTimeDifference') {
+    const times = extractTimes();
+    const difference = calculateTimeDifference(times);
     sendResponse(difference);
   }
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,32 @@
 document.getElementById('calculate-button').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'calculateDateDifference' }, (response) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'calculateTimeDifference' }, (response) => {
       const resultElement = document.getElementById('result');
-      resultElement.style.fontWeight = 'bold';
-      if (response && response.hours !== null && response.minutes !== null) {
+      const additionalTextElement = document.getElementById('additional-text');
+      const leavingHourTextElement = document.getElementById('leaving-hours');
+
+      resultElement.style.fontWeight = 'bold';  // Define o texto como negrito
+      additionalTextElement.style.fontWeight = 'bold'
+      leavingHourTextElement.style.fontWeight = 'bold'
+      additionalTextElement.innerText = ''
+      leavingHourTextElement.style.color = 'blue'
+      leavingHourTextElement.innerText = `Horário de Saída: ${response.leavingHours} horas ${response.leavingMinutes} minutos`;
+
+      if (response && response.hours !== null && response.minutes !== null && response.seconds !== null) {
         resultElement.innerText = `Horas Trabalhadas: ${response.hours} horas ${response.minutes} minutos`;
-        if (response.hours >= 8 && response.minutes > 48) {
+
+        // Alterar a cor com base na diferença em horas
+        if (response.hours <= 9) {
           resultElement.style.color = 'red';
+          additionalTextElement.style.color = 'red';
+          additionalTextElement.innerText = 'Keep Working !';
         } else {
           resultElement.style.color = 'green';
+          additionalTextElement.style.color = 'green';
+          additionalTextElement.innerText = 'Ihuuuuu!!! partiu! :) ';
         }
       } else {
-        resultElement.innerText = 'Não foi possível calcular a diferença. Verifique se as datas estão presentes.';
+        resultElement.innerText = 'Não foi possível calcular a diferença.';
         resultElement.style.color = 'black';
       }
     });
